541c1c2b319d373693d9324cf866a04bb7f107ef
catplotlib <43911345+Puja2402@users.noreply.github.com>
2021-07-30 21:05:23 +0530

    added resume

--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -3,7 +3,7 @@ import { IconContext } from "react-icons";
 import { GrTwitter, GrLinkedin, GrGithub } from "react-icons/gr";
 const Socials = () => {
   return (
-    <div>
+    <div className="cs">
       <h1>My Socials</h1>
       <IconContext.Provider value={{ color: "#6f6077", size: "3rem" }}>
         <div className="container-s">
@@ -15,6 +15,13 @@ const Socials = () => {
           </a>
           <a href="https://github.com/catplotlib">
             <GrGithub />
+          </a>
+        </div>
+        <div>
+          <a href="https://drive.google.com/file/d/1oziHS6qCXek92z27u-kN9mLBN-nWLBJQ/view">
+            <button style={{ marginBottom: "100px" }} className="btn">
+              My Resume
+            </button>
           </a>
         </div>
       </IconContext.Provider>
